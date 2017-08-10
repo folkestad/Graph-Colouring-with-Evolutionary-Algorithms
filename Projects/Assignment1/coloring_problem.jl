4fa5ddd818f38dcc890642afd29db935b9e9a38d
Eirik Folkestad <eirik.ek.folkestad@gmail.com>
2017-08-10 14:37:30 +1200

    Add avg fitness and new evolutionary tactics

--- Projects/Assignment1/coloring_problem.jl
+++ Projects/Assignment1/coloring_problem.jl
@@ -68,16 +68,16 @@ function generate(graph)
   return genotypes
 end
 
-# function mutate(genotype) # nothing extra
-#   genotype_length = length(genotype)
-#   mutated_genotype = copy(genotype)
-#   index = rand(1:genotype_length)
-#   mutation = rand(1:genotype_length)
-#   mutated_genotype[index] = mutation
-#   return mutated_genotype
-# end
+function mutate(genotype) # nothing extra
+  genotype_length = length(genotype)
+  mutated_genotype = copy(genotype)
+  index = rand(1:genotype_length)
+  mutation = rand(1:genotype_length)
+  mutated_genotype[index] = mutation
+  return mutated_genotype
+end
 
-function mutate(genotype, s)
+function mutate_s(genotype, s)
   genotype_length = length(genotype)
   mutated_genotype = copy(genotype)
   if s < 0.5  
@@ -125,15 +125,31 @@ function ev11(p, g, graph) # p = population size, g = number of generations, gra
   return pop # Return the population after modifying it over g generations
 end
 
-function evac(p, g, graph) # p = population size, g = number of generations, graph = graph of which to solve the coloring problem on
+function eva(p, g, graph) # p = population size, g = number of generations, graph = graph of which to solve the coloring problem on
   pop = fill([0], p) # Create array of size p which then becomes the population
   for i in 1:p # Fill array with generated solutions
     pop[i] = generate(graph)
   end
   for j in 1:g # Do changes to the population for g generations
-    s = (g-j+1)/g
     x = rand(1:p) # Select a random member of the population, ind_1
-    x_ = mutate(pop[x], s)  # Perform a modification to the random member x which will be called x_
+    s = (g-j+1)/g
+    x_ = mutate_s(pop[x], s) # Perform a modification to the random member x which will be called x_
+    y = rand(1:p) # Select a random member of the population, y
+    if fitness(graph, x_) < fitness(graph, pop[y]) # Fitness should be as small as possible in this case
+      pop[y] = x_
+    end
+  end
+  return pop # Return the population after modifying it over g generations
+end
+
+function evc(p, g, graph)
+  pop = fill([0], p) # Create array of size p which then becomes the population
+  for i in 1:p # Fill array with generated solutions
+    pop[i] = generate(graph)
+  end
+  for j in 1:g # Do changes to the population for g generations
+    x = rand(1:p) # Select a random member of the population, ind_1
+    x_ = mutate(pop[x])  # Perform a modification to the random member x which will be called x_
     y = rand(1:p) # Select a random member of the population, y
     p1 = rand(1:p)
     p2 = rand(1:p)
@@ -153,10 +169,80 @@ function evac(p, g, graph) # p = population size, g = number of generations, gra
   return pop # Return the population after modifying it over g generations
 end
 
-function do_and_print(graph, which)
+function evac(p, g, graph)
+  pop = fill([0], p) # Create array of size p which then becomes the population
+  for i in 1:p # Fill array with generated solutions
+    pop[i] = generate(graph)
+  end
+  for j in 1:g # Do changes to the population for g generations
+    s = (g-j+1)/g
+    x = rand(1:p) # Select a random member of the population, ind_1
+    x_ = mutate_s(pop[x], s)  # Perform a modification to the random member x which will be called x_
+    y = rand(1:p) # Select a random member of the population, y
+    p1 = rand(1:p)
+    p2 = rand(1:p)
+    c = crossover(pop[p1], pop[p2])
+    if fitness(graph, x_) < fitness(graph, pop[y]) # Fitness should be as small as possible in this case
+      pop[y] = x_
+    end
+    fit_c = fitness(graph, c)
+    fit_p1 = fitness(graph, pop[p1])
+    fit_p2 = fitness(graph, pop[p2])
+    if fit_c < fit_p1 && fit_p1 > fit_p2 
+      pop[p1] = c
+    elseif fit_c < fit_p2
+      pop[p2] = c
+    end
+  end
+  return pop # Return the population after modifying it over g generations
+end
+
+function evac_stm(p, g, graph) # p = population size, g = number of generations, graph = graph of which to solve the coloring problem on
+  n_save = 1
+  short_term_memory = fill(fill(-1, size(graph, 1)), n_save)
+  mem_index = 1
+  # for s in short_term_memory
+  #   println("$(s)")
+  # end
+  pop = fill([0], p) # Create array of size p which then becomes the population
+  for i in 1:p # Fill array with generated solutions
+    pop[i] = generate(graph)
+  end
+  for j in 1:g # Do changes to the population for g generations
+    s = (g-j+1)/g
+    x = rand(1:p) # Select a random member of the population, ind_1
+    x_ = mutate_s(pop[x], s)  # Perform a modification to the random member x which will be called x_
+    y = rand(1:p) # Select a random member of the population, y
+    p1 = rand(1:p)
+    p2 = rand(1:p)
+    c = crossover(pop[p1], pop[p2])
+    if !(x_ in short_term_memory) && fitness(graph, x_) < fitness(graph, pop[y]) # Fitness should be as small as possible in this case
+      pop[y] = x_
+      short_term_memory[mem_index] = x_
+      mem_index = (mem_index % n_save) + 1 
+    end
+    fit_c = fitness(graph, c)
+    fit_p1 = fitness(graph, pop[p1])
+    fit_p2 = fitness(graph, pop[p2])
+    if !(c in short_term_memory)
+      if fit_c < fit_p1 && fit_p1 > fit_p2 
+        pop[p1] = c
+        short_term_memory[mem_index] = c
+        mem_index = (mem_index % n_save) + 1 
+      elseif fit_c < fit_p2
+        pop[p2] = c
+        short_term_memory[mem_index] = c
+        mem_index = (mem_index % n_save) + 1
+      end
+    end
+  end
+  return pop # Return the population after modifying it over g generations
+end
+
+function do_and_print(graph, which, ea)
   p = 10
   g = 200
-  answer = evac(p, g, graph) # p = 50, g = 1000 solves every graph...
+  answer = ea(p, g, graph) # p = 50, g = 1000 solves every graph...
   best = answer[1]
   best_fit = fitness(graph, answer[1])
   @printf("Results for graph %s\n", which)
@@ -168,9 +254,26 @@ function do_and_print(graph, which)
     end
   end
   @printf("Best: %s, fitness %d\n", best, best_fit)
-  @printf("End of results for graph %s (p = %d, g = %d)\n\n", which, p, g)
+  @printf("End of results for graph %s (p = %d, g = %d)\n", which, p, g)
+  n_trials = 1000
+  results = fill(0, n_trials)
+  for n in 1:n_trials
+    answer = ea(p, g, graph)
+    best = answer[1]
+    best_fit = fitness(graph, answer[1])
+    for i in 1:length(answer)
+      if (best_fit > fitness(graph, answer[i]))
+        best = answer[i]
+        best_fit = fitness(graph, answer[i])
+      end
+    end
+    results[n] = best_fit
+  end
+  avg_result = sum(results) / n_trials
+  @printf("Avg best fitness over %d trials is %f\n\n", n_trials, avg_result)
 end
 
-# do_and_print(graph_crown(), "CROWN")
-# do_and_print(graph_peterson(), "PETERSON")
-do_and_print(graph_wheel(), "WHEEL")
+ea = evc
+do_and_print(graph_crown(), "CROWN (Optimal 2)", ea)
+do_and_print(graph_peterson(), "PETERSON (Optimal 3)", ea)
+do_and_print(graph_wheel(), "WHEEL (Optimal 4)", ea)
